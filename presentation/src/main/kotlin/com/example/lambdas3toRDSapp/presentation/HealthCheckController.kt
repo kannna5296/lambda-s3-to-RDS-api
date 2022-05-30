9a5cf0ed512838ce8f9967eb9ddae5132de663aa
kannna5296 <kannnahacks5296@gmail.com>
2022-05-30 20:05:45 +0900

    ヘルスチェック追加（Containerの生死確認する用）

--- presentation/src/main/kotlin/com/example/lambdas3toRDSapp/presentation/HealthCheckController.kt
+++ presentation/src/main/kotlin/com/example/lambdas3toRDSapp/presentation/HealthCheckController.kt
@@ -1,14 +1,15 @@
 package com.example.lambdas3toRDSapp.presentation
 
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthCheckController{
 
-    @PostMapping("/health_check")
+    @GetMapping("/health_check")
     fun register(): ResponseEntity<Unit> {
+        println("HEALTHCHECK!!!!!")
         return ResponseEntity.ok().body(null)
     }
 }
